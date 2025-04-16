57ddba9cb23d487f93e3417a09bbcaac48791160
Chosungjun <louis.cho333@gmail.com>
2025-04-16 23:37:40 +0900

    최상단 이미지 뉴스 슬라이드 기능 추가

--- lib/screen/healthtopic.dart
+++ lib/screen/healthtopic.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lifefit/const/colors.dart';
+
 
 class Healthtopic extends StatefulWidget {
   const Healthtopic({super.key});
@@ -11,6 +13,23 @@ class _HealthtopicState extends State<Healthtopic> {
   // 뉴스 테스트 이미지 에셋 경로
   final String test1Image = 'assets/img/test1.png';
   final String test2Image = 'assets/img/test2.png';
+
+  // 최상단 이미지 슬라이더/인디케이터
+  final List<int> pages = List.generate(4, (index) => index);
+  final PageController controller = PageController(initialPage: 0);
+  int curruntPage = 0;
+  final List<String> pageimgs = [ // 이미지 테스트
+    'assets/image1.jpg',
+    'assets/image2.jpg',
+    'assets/image3.jpg',
+    'assets/image4.jpg',
+  ];
+  final List<String> pageTexts = [ //텍스트 테스트
+    '첫 번째 페이지 텍스트',
+    '두 번째 페이지 텍스트 내용',
+    '세 번째 페이지의 아주 긴 텍스트입니다. 이 텍스트는 컨테이너 너비에 맞춰 자동으로 줄바꿈될 것입니다.',
+    '네 번째 페이지 짧은 텍스트',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -29,46 +48,93 @@ class _HealthtopicState extends State<Healthtopic> {
         ],
       ), // 상단 GNB
 
-
       body: SingleChildScrollView(
         padding: EdgeInsets.symmetric(horizontal: 20), // 마진
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // 최 상단 이미지 및 텍스트
-            Stack(
-              alignment: Alignment.center,
-              children: [
-                ClipRRect( // 이미지 모서리를 둥글게 처리하기 위해 ClipRRect 사용
-                  borderRadius: BorderRadius.circular(8.0),
-                  child: Image.asset(test1Image,
-                      width: double.infinity,
-                      fit: BoxFit.cover),
-                ),
-                Image.asset(test1Image,
-                    width: double.infinity, fit: BoxFit.cover),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Align(
-                      alignment: Alignment.bottomLeft, // 텍스트 좌측 하단정렬
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Text(
-                          '오늘 저녁은 뭘 먹지? 맛있고 건강하게 즐기는 특별한 레시피',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.bold,
+
+            //최상단 이미지 슬라이드 뉴스
+            Container(
+              width: double.infinity,
+              height: 320,
+              child: PageView.builder(
+                controller: controller,
+                itemCount: pageimgs.length,
+                onPageChanged: (page) {
+                  setState(() {
+                    curruntPage = page;
+                    print(page);
+                  });
+                },
+                itemBuilder: (context, index) {
+                  return Padding( // 카드 주변에 약간의 간격을 주기 위해 Padding 추가
+                    padding: const EdgeInsets.symmetric( vertical: 6.0),
+                    child: Card(
+                      shape: RoundedRectangleBorder( // 카드 모서리 둥글게 설정
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                      elevation: 4.0, // 그림자 효과
+                      child: Stack(
+                        fit: StackFit.expand, // Stack이 Card의 크기에 맞춰지도록 설정
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(16.0),
+                            child: Image.asset(
+                              pageimgs[index],
+                              width: double.infinity,
+                              height: double.infinity,
+                              fit: BoxFit.cover,
+                            ),
                           ),
-                        ),
+                          Positioned(
+                            left: 16,
+                            bottom: 16,
+                            right: 16,
+                            child: Text(
+                              pageTexts[index], // 해당 페이지의 텍스트를 가져옵니다.
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ],
-                )
+                  );
+                },
+              ),
+            ), // 카드
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                for (num i = 0; i < pageimgs.length; i++) // pageimgs의 길이에 맞춰 인디케이터 생성
+                  Container(
+                    margin: EdgeInsets.all(3),
+                    width: 10,
+                    height: 10,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: curruntPage == i
+                          ? Colors.blue
+                          : Colors.grey.withOpacity(0.5),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.12),
+                          blurRadius: 4,
+                          spreadRadius: 1,
+                        ),
+                      ],
+                    ),
+                  ),
               ],
-            ),
+            ), // 인디케이터
             SizedBox(height: 48.0),
+
             // 실시간 토픽 섹션
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 8.0),
@@ -79,45 +145,54 @@ class _HealthtopicState extends State<Healthtopic> {
             SizedBox(
                 height: 424.0,
                 child: ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
                     scrollDirection: Axis.vertical,
                     itemCount: 4, // 예시 아이템 수
                     itemBuilder: (context, index) {
-                      return Card(shape: ContinuousRectangleBorder(// [모서리 살짝 둥글게 사용]
-                        borderRadius: BorderRadius.circular(16.0), // [둥글기 설정]
-                      ),
-                        elevation: 0.0, //그림자 깊이
-                        color: Colors.white, // [색상 지정]
+                      return Card(
+                        shape: ContinuousRectangleBorder(
+                          // [모서리 살짝 둥글게 사용]
+                          borderRadius: BorderRadius.circular(16.0), // [둥글기 설정]
+                        ),
+                        elevation: 0.0,
+                        //그림자 깊이
+                        color: Colors.white,
+                        // [색상 지정]
                         margin: EdgeInsets.symmetric(vertical: 8.0),
-                            child: Row(
-                              children: [
-                                ClipRRect( // 이미지 모서리를 둥글게 처리하기 위해 ClipRRect 사용
-                                  borderRadius: BorderRadius.circular(8.0),
-                                  child: Image.asset(
-                                    test1Image,
-                                    width: 118.0,
-                                    height: 90.0,
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                                SizedBox(width: 8.0),
-                                Expanded(
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text('피클·오이지 즐겨 먹었는데…뜻밖의 연구 결과에',
-                                          style: TextStyle(fontSize: 15.0,
-                                              fontWeight: FontWeight.bold), overflow: TextOverflow.ellipsis,
-                                          maxLines: 2),
-                                      SizedBox(height: 8.0),
-                                      Text('한국뉴스 | 25.04.01'),
-                                    ],
-                                  ),
-                                ),
-                              ],
+                        child: Row(
+                          children: [
+                            ClipRRect(
+                              // 이미지 모서리를 둥글게 처리하기 위해 ClipRRect 사용
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: Image.asset(
+                                test1Image,
+                                width: 118.0,
+                                height: 90.0,
+                                fit: BoxFit.cover,
+                              ),
                             ),
-                          );
+                            SizedBox(width: 8.0),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('피클·오이지 즐겨 먹었는데…뜻밖의 연구 결과에',
+                                      style: TextStyle(
+                                          fontSize: 15.0,
+                                          fontWeight: FontWeight.bold),
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 2),
+                                  SizedBox(height: 8.0),
+                                  Text('한국뉴스 | 25.04.01'),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
                     })),
             SizedBox(height: 48.0),
+
             // 식사 추천 섹션
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 8.0),
@@ -131,21 +206,25 @@ class _HealthtopicState extends State<Healthtopic> {
                     scrollDirection: Axis.horizontal,
                     itemCount: 5, // 예시 아이템 수
                     itemBuilder: (context, index) {
-                      return Card(shape: ContinuousRectangleBorder(// [모서리 살짝 둥글게 사용]
-                        borderRadius: BorderRadius.circular(16.0), // [둥글기 설정]
-                      ),
+                      return Card(
+                        shape: ContinuousRectangleBorder(
+                          // [모서리 살짝 둥글게 사용]
+                          borderRadius: BorderRadius.circular(16.0), // [둥글기 설정]
+                        ),
                         elevation: 4.0, //그림자 깊이
                         color: Colors.white, // [색상 지정]
 
                         child: Padding(
                           padding: const EdgeInsets.all(8.0),
                           child: Column(
-                            children: [ClipRRect( // 이미지 모서리를 둥글게 처리하기 위해 ClipRRect 사용
-                              borderRadius: BorderRadius.circular(8.0),
-                              child: Image.asset(test1Image,
-                                  width: 154.0,
-                                  height: 154.0,
-                                  fit: BoxFit.cover),
+                            children: [
+                              ClipRRect(
+                                // 이미지 모서리를 둥글게 처리하기 위해 ClipRRect 사용
+                                borderRadius: BorderRadius.circular(8.0),
+                                child: Image.asset(test1Image,
+                                    width: 154.0,
+                                    height: 154.0,
+                                    fit: BoxFit.cover),
                               ),
                               SizedBox(height: 8.0),
                               Text('상큼한 샐러드 레시피', textAlign: TextAlign.left),
@@ -157,6 +236,7 @@ class _HealthtopicState extends State<Healthtopic> {
                       );
                     })),
             SizedBox(height: 48.0),
+
             // 영상 섹션
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 8.0),
@@ -171,7 +251,8 @@ class _HealthtopicState extends State<Healthtopic> {
                 itemCount: 5, // 예시 아이템 수
                 itemBuilder: (context, index) {
                   return Card(
-                    shape: ContinuousRectangleBorder(// [모서리 살짝 둥글게 사용]
+                    shape: ContinuousRectangleBorder(
+                      // [모서리 살짝 둥글게 사용]
                       borderRadius: BorderRadius.circular(16.0), // [둥글기 설정]
                     ),
                     elevation: 0.0, //그림자 깊이
@@ -183,7 +264,8 @@ class _HealthtopicState extends State<Healthtopic> {
                         Stack(
                           alignment: Alignment.center,
                           children: [
-                            ClipRRect( // 이미지 모서리를 둥글게 처리하기 위해 ClipRRect 사용
+                            ClipRRect(
+                              // 이미지 모서리를 둥글게 처리하기 위해 ClipRRect 사용
                               borderRadius: BorderRadius.circular(4.0),
                               child: Image.asset(test1Image,
                                   width: 290.0,
@@ -203,7 +285,8 @@ class _HealthtopicState extends State<Healthtopic> {
                               children: [
                                 Text('\'저탄고지\' 다이어트 효과 있을까? 제대로 알고 시작하세요',
                                     style:
-                                        TextStyle(fontWeight: FontWeight.bold),overflow: TextOverflow.ellipsis,
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                    overflow: TextOverflow.ellipsis,
                                     maxLines: 2),
                                 SizedBox(height: 8.0),
                                 Text('△△△ 영양사'),
@@ -218,6 +301,7 @@ class _HealthtopicState extends State<Healthtopic> {
               ),
             ),
             SizedBox(height: 48.0),
+
             // 전문가 칼럼 섹션
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 8.0),
@@ -228,38 +312,42 @@ class _HealthtopicState extends State<Healthtopic> {
             SizedBox(
                 height: 280.0,
                 child: ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
                     scrollDirection: Axis.vertical,
                     itemCount: 2, // 예시 아이템 수
                     itemBuilder: (context, index) {
-                      return Card(shape: ContinuousRectangleBorder(// [모서리 살짝 둥글게 사용]
-                        borderRadius: BorderRadius.circular(16.0), // [둥글기 설정]
-                      ),
+                      return Card(
+                        shape: ContinuousRectangleBorder(
+                          // [모서리 살짝 둥글게 사용]
+                          borderRadius: BorderRadius.circular(16.0), // [둥글기 설정]
+                        ),
                         elevation: 2.0, //그림자 깊이
                         color: Colors.white, // [색상 지정]
-                          child: Row(
-                            children: [
-                              ClipRRect( // 이미지 모서리를 둥글게 처리하기 위해 ClipRRect 사용
-                                borderRadius: BorderRadius.circular(8.0),
-                                child: Image.asset(test1Image,
-                                    width: 80.0,
-                                    height: 130.0,
-                                    fit: BoxFit.cover),
+                        child: Row(
+                          children: [
+                            ClipRRect(
+                              // 이미지 모서리를 둥글게 처리하기 위해 ClipRRect 사용
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: Image.asset(test1Image,
+                                  width: 80.0,
+                                  height: 130.0,
+                                  fit: BoxFit.cover),
+                            ),
+                            SizedBox(width: 8.0),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('\'물 단식\' 다이어트 유행? 건강 지키려면 차라리 \'이것\'',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold)),
+                                  Text('OOO 박사'),
+                                ],
                               ),
-                              SizedBox(width: 8.0),
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text('\'물 단식\' 다이어트 유행? 건강 지키려면 차라리 \'이것\'',
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold)),
-                                    Text('OOO 박사'),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        );
+                            ),
+                          ],
+                        ),
+                      );
                     })),
             SizedBox(height: 16.0),
           ],
