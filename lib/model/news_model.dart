dbecc2e1a2cf10fa71af6b4a3455629ff77c76a6
Chosungjun <louis.cho333@gmail.com>
2025-04-30 09:09:32 +0900

    뉴스/토픽 모델

---
+++ lib/model/news_model.dart
@@ -0,0 +1,30 @@
+// jsonplaceholder를 사용한 테스트
+
+class Photo {
+  final int albumId;
+  final int id;
+  final String title;
+  final String url;
+  final String thumbnailUrl;
+
+  Photo({
+    required this.albumId,
+    required this.id,
+    required this.title,
+    required this.url,
+    required this.thumbnailUrl,
+  });
+
+  factory Photo.fromJson(Map<String, dynamic> json) {
+    return Photo(
+      albumId: json['albumId'] ?? 0,
+      id: json['id'] ?? 0,
+      title: json['title'] ?? '',
+      url: json['url'] ?? '',
+      thumbnailUrl: json['thumbnailUrl'] ?? '',
+    );
+  }
+
+  // 데이테만 받기 때문에 tojson 사용x
+
+}
